32ce2eef267012202316a16e8cf00681d2599017
RyianDzuhri <muhammadryiandzuhri@gmail.com>
2026-07-24 23:51:20 +0800

    update

--- profil-desa/astro.config.mjs
+++ profil-desa/astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://ryiandzuhri.github.io',
+  base: '/hukaea',
+
   vite: {
     plugins: [tailwindcss()]
   }
